438129216ae0ff0b3fbdd318a575f0d60993d50f
oahmed105 <oahmed10598@gmail.com>
2023-07-05 19:54:16 -0400

    Completed the Ifs tests

--- src/java/WriteIFs.java
+++ src/java/WriteIFs.java
@@ -7,44 +7,57 @@
  */
 public class WriteIFs
 {
- 
+
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        if (isAlive(player1)) {
+
+        } else {
+            displayGameOver(player1);
+        }
+
     }
-    
+
     public String thermoSTAT(int room) {
-        // Write an IF statement that checks the 
-        // “temperature(room)” and if that check is less than 70, 
+        // Write an IF statement that checks the
+        // “temperature(room)” and if that check is less than 70,
         // calls “heatOn()” else calls “coolOn()”
+        if (tempurature(room) < 70) {
+            heatOn();
+        } else {
+            coolOn();
+        }
 
 
-        
         return this.ss;
     }
 
     public void fireplaceControl(Object fireplace1) {
-        // Write an IF statement that checks 
-        // “outsideTemp()” is less than 50 
-        // AND 
-        // “insideTemp()” is less than 62, 
+        // Write an IF statement that checks
+        // “outsideTemp()” is less than 50
+        // AND
+        // “insideTemp()” is less than 62,
         // calls “startAFire(fireplace1)”
-
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
     }
 
     public void checkFuel(double fuelLevel) {
-        // Write an IF statement that checks “fuelLevel” 
+        // Write an IF statement that checks “fuelLevel”
         // and if that check is less than 0.08, calls “refuel()”
-
+        if (fuelLevel < 0.08) {
+            refuel();
+        }
     }
 
 
-    
+
     /**
      *  Pay no attention to the code below this point.
      *
-     * 
+     *
      * instance variables
      */
     public int x;
@@ -81,7 +94,7 @@ public class WriteIFs
     private void coolOn() {
         this.ss = "cooling";
     }
- 
+
     private int insideTemp() {
         return oo1;
     }
